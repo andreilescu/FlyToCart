5a810aff4928a9610900743f1e2678bb45fe9b1b
Andrei Lescu <andrei@linux.fritz.box>
2016-09-26 18:14:25 +0200

    some changes

---
+++ resources/js/extractInSingleComponent.js
@@ -0,0 +1,59 @@
+(function($){
+	/*	
+	function initialize() {
+		// create a product 
+		var product1 = new Product("Big Air Tyro 24", 1599, "&euro;", "../resources/images/Big_Air_Tyro_24.png");
+		var product2 = new Product("Contrail 9.0", 1399, "&euro;", "../resources/images/Contrail_9_0.png");
+		var product3 = new Product("Fastlane 8.0 29", 2999, "&euro;", "../resources/images/Fastlane_8_0_29.png");
+		var product4 = new Product("Fastlane MGN 29", 1899, "&euro;", "../resources/images/Fastlane_MGN_29.png");
+		
+		//create array of products
+		var products = new Array(); 
+		products[0] = product1;
+		products[1] = product2;
+		products[2] = product3;
+		products[3] = product4;
+		
+		// get template
+		var template = $("#hidden-template").html();
+		// html array
+		var productTemplates = new Array();
+	
+		for(var i=0; i<products.length; i++) {
+			$(".products-div").append(createTemplate(template, products[i]));
+		}
+	}
+	
+	function createTemplate(template, product) {
+		
+		// set image src
+		$(template).find("img")
+				   .attr("src", product.src);
+		
+		// set name
+		$(".product-name").text(product.name);
+		
+		// set product currency
+		$("#product-currency").text(product.currency);
+		// set product price
+		$("#product-price").text(product.price);
+				   
+		console.log($(template).html());
+		return $(template).html();
+				   
+	}
+	
+	function Product(name, price, currency, src) {
+		this.name = name;
+		this.price = price;
+		this.currency = currency;
+		this.src = src;		
+	}
+	
+	*/
+	//initialize();
+	
+	
+	
+})(jQuery);
+
